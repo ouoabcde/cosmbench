765996589a6fb2535c3fa86187d65d48af3778a7
Mansub Song <ssongman1029@gmail.com>
2025-01-09 17:34:29 +0900

    feat: write node log into output.log and print timestamp

--- 92_run.sh
+++ 92_run.sh
@@ -5,4 +5,14 @@ source ./run_env.sh
 INDEX=$1
 CURRENT_DATA_DIR=$TESTDIR/node$INDEX
 echo "$BINARY start --home $CURRENT_DATA_DIR"
-$BINARY start --home $CURRENT_DATA_DIR
+#$BINARY start --home $CURRENT_DATA_DIR
+
+
+rm -f output$INDEX.log
+
+# committed state라는 문장이 들어있는 문장에 Unix millisecond timestamp 출력
+$BINARY start --home $CURRENT_DATA_DIR 2>&1 | while IFS= read -r line; do
+  if [[ "$line" == *"committed state"* ]]; then
+    echo "$(date '+%s%3N') $line"
+  fi
+done >> output$INDEX.log
